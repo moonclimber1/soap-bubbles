353b7ec8a8eecc3f514421f82ab7218be03a42be
moonclimber1 <55745851+moonclimber1@users.noreply.github.com>
2021-03-15 02:50:25 +0100

    Socket IO Server & Docker

---
+++ socket-server/index.js
@@ -0,0 +1,21 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3333, () => {
+  console.log('listening on *:3333');
+});
