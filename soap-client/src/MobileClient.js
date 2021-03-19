e281b24df520b8c3ca2b1c047085085611a57d09
moonclimber1 <55745851+moonclimber1@users.noreply.github.com>
2021-03-19 17:40:06 +0100

    Update MobileClient.js

--- soap-client/src/MobileClient.js
+++ soap-client/src/MobileClient.js
@@ -16,6 +16,82 @@ class MobileClient extends React.Component {
       console.log("🚀 ~ file: MobileClient.js ~ line 17 ~ MobileClient ~ socket.on ~ this.currentBubble", this.currentBubble);
       this.sendBubbleToWorld();
     });
+
+    /**
+     * Create global accessible variables that will be modified later
+     */
+    var audioContext = null;
+    var meter = null;
+    var rafID = null;
+    var mediaStreamSource = null;
+
+    // Retrieve AudioContext with all the prefixes of the browsers
+    window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+    // Get an audio context
+    audioContext = new AudioContext();
+
+    /**
+     * Callback triggered if the microphone permission is denied
+     */
+    function onMicrophoneDenied() {
+      alert("Stream generation failed.");
+    }
+
+    /**
+     * Callback triggered if the access to the microphone is granted
+     */
+    function onMicrophoneGranted(stream) {
+      // Create an AudioNode from the stream.
+      mediaStreamSource = audioContext.createMediaStreamSource(stream);
+      // Create a new volume meter and connect it.
+      meter = createAudioMeter(audioContext);
+      mediaStreamSource.connect(meter);
+
+      // Trigger callback that shows the level of the "Volume Meter"
+      onLevelChange();
+    }
+
+    /**
+     * This function is executed repeatedly
+     */
+    function onLevelChange(time) {
+      // check if we're currently clipping
+
+      if (meter.checkClipping()) {
+        console.warn(meter.volume);
+      } else {
+        console.log(meter.volume);
+      }
+
+      // set up the next callback
+      rafID = window.requestAnimationFrame(onLevelChange);
+    }
+
+    // Try to get access to the microphone
+    try {
+      // Retrieve getUserMedia API with all the prefixes of the browsers
+      navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+
+      // Ask for an audio input
+      navigator.getUserMedia(
+        {
+          audio: {
+            mandatory: {
+              googEchoCancellation: "false",
+              googAutoGainControl: "false",
+              googNoiseSuppression: "false",
+              googHighpassFilter: "false",
+            },
+            optional: [],
+          },
+        },
+        onMicrophoneGranted,
+        onMicrophoneDenied
+      );
+    } catch (e) {
+      alert("getUserMedia threw exception :" + e);
+    }
   }
 
   createBubble(socketID) {
@@ -35,7 +111,7 @@ class MobileClient extends React.Component {
       return;
     }
     // Emit Message
-    this.socket.emit('send to world', this.currentBubble);
+    this.socket.emit("send to world", this.currentBubble);
   }
 
   render() {
