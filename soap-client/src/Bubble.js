d715e17e8ff9f199237c38ea7e165255e32c7163
Jonas Wolter <jonas.wolter.ch@gmail.com>
2021-04-02 14:21:16 +0200

    Snowflakes * . *

--- soap-client/src/Bubble.js
+++ soap-client/src/Bubble.js
@@ -3,7 +3,7 @@
 import Victor from 'victor';
 import Q5 from "./assets/q5.js";
 
-const CIRCLE_RADIUS = 100;
+const CIRCLE_RADIUS = 300;
 const NOISE_OFFSET = 200;
 
 const DRAG_FACTOR = 0.01;
@@ -29,6 +29,8 @@ class Bubble {
     const C1 =  STATIC_Q5.color(34,193,195);
     const C2 = STATIC_Q5.color(253,187,45);
     this.color = STATIC_Q5.lerpColor(C1, C2, Math.random());
+
+    this.t = 0
   }
 
   static createFromBubble(bubble){
@@ -79,10 +81,54 @@ class Bubble {
   draw(q5){
     q5.fill(this.color)
     q5.noStroke()
-    let x = this.pos.x + this.noiseOffset.x
-    let y = this.pos.y + this.noiseOffset.y
-    q5.ellipse(x, y, CIRCLE_RADIUS*2, CIRCLE_RADIUS*2)
+    let xPos = this.pos.x + this.noiseOffset.x
+    let yPos = this.pos.y + this.noiseOffset.y
+    // q5.ellipse(xPos, yPos, CIRCLE_RADIUS*2, CIRCLE_RADIUS*2)
+
+    // Draw Wobbly Circle
+
+    
+
+    const POINTS_NUMBER = 200;
+    const step = q5.TWO_PI / POINTS_NUMBER;
+    let x, y, noiseMultiplier
+    q5.beginShape()
+    for (let angle = 0; angle < q5.TWO_PI; angle += step) {
+        x = q5.cos(angle)
+        y = q5.sin(angle)
+        const noise = q5.map(q5.noise(x,y), 0,1, -1, 1)
+        const unitVec = new Victor(x,y)
+        const point = new Victor(x,y).multiplyScalar(CIRCLE_RADIUS * noise)
+        
+
+
+        q5.vertex(xPos + point.x, yPos + y + point.y)
+    }
+    q5.endShape(q5.CLOSE)
+
+
+    // const nPoints = 300
+    // const angle = q5.TWO_PI / nPoints
+    // this.t += 0.01;
+
+    // let x,y,p,n;
+ 
+    // q5.beginShape()
+    // // q5.strokeWeight(4)
+    // for (let i = 0; i <= nPoints; i +=1 ) {
+    //     x = q5.cos(angle * i) * CIRCLE_RADIUS
+    //     console.log("🚀 ~ file: Bubble.js ~ line 100 ~ Bubble ~ draw ~ x", x)
+    //     y = q5.sin(angle * i) * CIRCLE_RADIUS 
+    //     console.log("🚀 ~ file: Bubble.js ~ line 102 ~ Bubble ~ draw ~ y", y)
+    //     p = new Victor(x,y).normalize()
+    //     n = q5.map(q5.noise(p.x + this.t, p.y + this.t), 0, 1, 70, 120)
+    //     p.multiplyScalar(n*2)
+    //     q5.vertex(xPos + p.x, yPos + p.y)
+    // }
+    // q5.endShape(q5.CLOSE)
   }
+
+
 }
 
 export default Bubble;
