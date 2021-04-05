f1bfb17bc47432a3262a6b1ab5a21f0067e68d3b
Jonas Wolter <jonas.wolter.ch@gmail.com>
2021-04-05 22:26:08 +0200

    bubble animation yeah

--- soap-client/src/Bubble.js
+++ soap-client/src/Bubble.js
@@ -4,7 +4,7 @@ import Q5 from "./assets/q5.js";
 const CIRCLE_RADIUS = 100;
 const NOISE_OFFSET = 200;
 
-const POINTS_NUMBER = 200;
+const POINTS_NUMBER = 100;
 const WOBBLE_RADIUS = CIRCLE_RADIUS / 3
 
 const DRAG_FACTOR = 0.01;
@@ -42,8 +42,7 @@ class Bubble {
   }
 
   static createFromBubble(bubble) {
-    const newBubble = new Bubble(bubble.id, bubble.pos, bubble.pullEnabled, bubble.pullPoint);
-    newBubble.imagePath = bubble.imagePath;
+    const newBubble = new Bubble(bubble.id, bubble.pos, bubble.pullEnabled, bubble.pullPoint, bubble.imagePath);
     newBubble.velocity = new Victor(bubble.velocity.x, bubble.velocity.y);
     newBubble.noiseOffset = bubble.noiseOffset;
     newBubble.frame = bubble.frame;
@@ -94,31 +93,16 @@ class Bubble {
 
     // Draw Wobbly Circle
 
-    q5.fill(this.color);
-    q5.noStroke();
+    // Get canvas context from Q5
+    const ctx = q5.drawingContext;
 
-    const shapeLength = (CIRCLE_RADIUS + WOBBLE_RADIUS) * 2
-    const shape = q5.createGraphics(shapeLength, shapeLength)
-    shape.fill(0)
-    shape.noStroke();
+    // Save state
+    ctx.save();
 
+    
     const step = q5.TWO_PI / POINTS_NUMBER;
     let x, y, noise;
     let p = new Victor(0, 0);
-    // shape.beginShape();
-    // for (let angle = 0; angle < q5.TWO_PI; angle += step) {
-    //   p.x = q5.cos(angle);
-    //   p.y = q5.sin(angle);
-    //   noise = q5.map(q5.noise(p.x * 0.25 + 1 + this.frame / 500, p.y * 0.25 + 1 + this.frame / 500),0,1, -WOBBLE_RADIUS, WOBBLE_RADIUS);
-    //   p.multiplyScalar(CIRCLE_RADIUS + noise);
-    //   shape.vertex(shapeLength/2 + p.x, shapeLength/2 + p.y);
-    // }
-    // shape.endShape(q5.CLOSE);
-    // q5.image(shape, this.pos.x -shapeLength/2 , this.pos.y - shapeLength/2)
-
-
-    const ctx = q5.drawingContext;
-    ctx.save();
 
     ctx.beginPath();
     for (let angle = 0; angle < q5.TWO_PI; angle += step) {
@@ -128,9 +112,9 @@ class Bubble {
       p.multiplyScalar(CIRCLE_RADIUS + noise);
 
       if(angle ===  0){
-        ctx.moveTo(shapeLength/2 + p.x, shapeLength/2 + p.y)
+        ctx.moveTo(xPos + p.x, yPos + p.y)
       }else{
-        ctx.lineTo(shapeLength/2 + p.x, shapeLength/2 + p.y)
+        ctx.lineTo(xPos + p.x, yPos + p.y)
       }
     }
     ctx.closePath();
@@ -142,13 +126,12 @@ class Bubble {
 
     // Draw clipped image
     ctx.clip()
-    if (this.image) ctx.drawImage(this.image, 0,0, shapeLength, shapeLength)
+    const imgWidth = (CIRCLE_RADIUS + WOBBLE_RADIUS) * 2
+    if (this.image) ctx.drawImage(this.image, xPos - imgWidth/2 ,  yPos - imgWidth/2, imgWidth, imgWidth)
 
     // Reset clip
     ctx.restore();
   }
-    
-   
 }
 
 export default Bubble;
